524f45111c427fd5e12a60affcd705fd07cbe46b
Sean Watkins <seanlw@gmail.com>
2020-05-11 15:27:34 -0500

    Handle correct loading of dev tools

--- src/main/app-window.ts
+++ src/main/app-window.ts
@@ -65,17 +65,21 @@ export class AppWindow {
     })
 
     if (__DEV__) {
-      this.window.webContents.openDevTools()
       this.window.loadURL(`http://localhost:53500`)
+      this.window.webContents.on('did-frame-finish-load', () => {
+        this.window.webContents.once('devtools-opened', () => {
+          this.window.focus()
+        })
+        this.window.webContents.openDevTools()
+        // const {
+        //   default: installExtension,
+        //   REACT_DEVELOPER_TOOLS
+        // } = require('electron-devtools-installer')
 
-      const {
-        default: installExtension,
-        REACT_DEVELOPER_TOOLS
-      } = require('electron-devtools-installer')
-
-      installExtension(REACT_DEVELOPER_TOOLS)
-        .then((name: string) => console.log(`Added Extension:  ${name}`))
-        .catch((err: Error) => console.log('An error occurred: ', err))
+        // installExtension(REACT_DEVELOPER_TOOLS)
+        //   .then((name: string) => console.log(`Added Extension:  ${name}`))
+        //   .catch((err: Error) => console.log('An error occurred: ', err))
+      })
     }
     else {
       this.window.loadURL(formatUrl({
